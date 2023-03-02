84b783f2f51304745166cbbdc0d5c4310dea98b3
tanvir1111 <tanvirahmed1024@gmail.com>
2023-03-02 12:30:59 +0600

    dockerignore & gitignore added

--- app.js
+++ app.js
@@ -1,29 +1,145 @@
 import { createServer } from 'http'
-import { readLog, writeLog } from './fileOperations.js'
+import { readLog, writeLog, exists, getVideo } from './fileOperations.js'
+import os from 'os'
+import fs from 'fs'
+let root = os.homedir();
+
 const server = createServer((req, res) => {
-    writeLog(req.url + ' ' + req.method)
     if (req.method !== 'GET') {
+        writeLog(req.url + ' ' + req.method)
         res.writeHead(405, { 'Content-Type': 'application/json' })
         return res.end(JSON.stringify({ message: 'invalid method' }))
     }
 
     if (req.url === '/') {
-        res.writeHead(200, { 'Content-type': 'text/*' })
-        return res.end("HELLO WORLD")
+        writeLog(req.url + ' ' + req.method)
+
+        res.writeHead(200, { 'Content-type': 'text/html' })
+        fs.readFile('index.html', (err, data) => {
+            res.write(data)
+            return res.end()
+        })
+        // return res.end("index.html")
     }
 
 
     else if (req.url === '/api/getLogs') {
         res.writeHead(200, { 'Content-Type': 'text/*' })
+        writeLog(req.url + ' ' + req.method)
+
+
         return res.end(readLog())
-    } else {
+    }
+    else if (req.url == '/api/video') {
+
+
+        const range = req.headers.range;
+        if (!range) {
+            res.status(400).send("Requires Range header");
+        }
+        writeLog(req.url + ' ' + req.method + range)
+
+        const videoPath = "bigbuck.mp4";
+        const videoSize = fs.statSync("bigbuck.mp4").size;
+        const CHUNK_SIZE = 10 ** 6;
+        const start = Number(range.replace(/\D/g, ""));
+        const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+        const contentLength = end - start + 1;
+        const headers = {
+            "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+            "Accept-Ranges": "bytes",
+            "Content-Length": contentLength,
+            "Content-Type": "video/mp4",
+        };
+
+        res.writeHead(206, headers);
+
+        return getVideo(videoPath, start, end).pipe(res);
+
+
+
+    }
+    else if (exists(root + req.url)) {
+        let filePath = root + req.url;
+        let stats = fs.statSync(filePath)
+        res.writeHead(200, {
+            'Content-Type': 'text/*',
+            'Content-Length': stats.size
+        })
+
+        let readStream = fs.createReadStream(root + req.url)
+        // readStream.pipe(res)
+        // let writeStream = fs.createWriteStream('./text2.txt', {})
+        // readStream.pipe(writeStream)
+        var completed = 0
+
+
+
+        readStream.on('data', (chunk) => {
+            // console.log(chunk);
+            // console.log(chunk.length)
+            console.log(`${completed} / ${stats.size}`)
+            completed += chunk.length
+            res.write(`<br/> <b>${completed} / ${stats.size}</b><br/> `)
+
+            res.write(chunk.toString())
+            readStream.pause()
+
+            setTimeout(() => { readStream.resume(); }, 1000)
+            // res.write(chunk.length.toString())
+        }).on('end', () => readStream.close())
+
+    }
+    else {
         res.writeHead(404, { 'Content-Type': 'application/json' })
         return res.end(JSON.stringify({ message: 'Invalid url' }))
     }
 })
 
-const PORT = process.env.PORT || 5000
+
+const PORT = 3000
+
 server.listen(PORT, () => {
-    writeLog("server started at port " + PORT);
-    console.log('server listening to ' + PORT)
+    writeLog(`server started at ${PORT}`)
 })
+
+
+// const root = os.homedir()
+
+// app.get('/', (req, res) => {
+//     res.send('hello')
+// })
+// app.get('*', (req, res) => {
+//     let stats = fs.statSync(root + req.originalUrl)
+//     console.log(stats.size);
+//     res.writeHead(200, {
+//         'Content-Type': 'text/*',
+//         'Content-Length': stats.size
+//     })
+
+//     let readStream = fs.createReadStream(root + req.originalUrl)
+//     // readStream.pipe(res)
+//     // // let writeStream = fs.createWriteStream('./text2.txt', {})
+//     // // readStream.pipe(writeStream)
+//     readStream.on('data', (chunk) => {
+//         console.log(chunk);
+//         console.log(chunk.length)
+//         res.write(chunk.toString())
+//         res.write(chunk.length.toString())
+//     })
+
+
+//     // console.log(data)
+//     return res.status(200)
+// })
+// app.listen(3000, () => {
+//     console.log("listening to 3000");
+// })
+
+
+// const PORT = process.env.PORT || 5000
+// server.listen(PORT, () => {
+//     writeLog("server started at port " + PORT);
+//     console.log('server listening to ' + PORT)
+// })
--- fileOperations.js
+++ fileOperations.js
@@ -11,3 +11,23 @@ export const readLog = () => {
     return fs.readFileSync('./log.txt', 'utf8')
 
 }
+
+export const exists = (path) => {
+    console.log(path);
+    return fs.existsSync(path)
+
+}
+
+export const fileType = (path) => {
+    return path.split('.').pop();
+
+}
+
+export const getVideo = (videoPath, start, end) => {
+
+
+
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+
+    return videoStream;
+}
